374384eef82607cdd9ce37a795f241733f3be137
antogorosito <40251595+antogorosito@users.noreply.github.com>
2019-10-14 20:02:11 -0300

    Add files via upload

---
+++ faq.php
@@ -0,0 +1,87 @@
+<?php 
+	session_start();
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+		<link rel="stylesheet" href="estilos.css" type="text/css" media="screen"/>
+		<title>Supermercado SAV</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+	</head>
+	<body>
+		<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+			  <a class="navbar-brand" href="index.php">
+				<img src="titulo.jpg" alt="Logo" style="width:15vw;" />
+			  </a>
+			  <ul class="navbar-nav">
+				<li class="nav-item">
+				  <a class="nav-link color" href="homeAdmin.php">Home</a>
+				</li>
+				<li class="nav-item">
+				  <a class="nav-link" href="abmUsuarios.php">Usuarios</a>
+				</li>
+				<li class="nav-item">
+					<div class='btn-group'>
+						<a  class='nav-link dropdown-toggle' data-toggle='dropdown'>Pedidos</a>
+						<div class='dropdown-menu'>
+							<a class='dropdown-item' href='pedidos.php'>Panel de pedidos</a>
+							<a class='dropdown-item' href='informesVenta.php'>Informes de venta</a>
+						</div>
+					</div>
+				</li>
+				<li class="nav-item">
+				  <a class="nav-link" href="abmProductos.php">Productos</a>
+				</li>
+				<li class="nav-item">		
+				<?php	
+				if(!isset($_SESSION["usuario"]))
+				{ 
+					echo "<a class='btn btn-outline-success' href='login.php'>Iniciar sesion</a> ";
+				}
+				else
+				{
+					echo "<div class='btn-group'>
+					<button type='button' class='btn btn-outline-success dropdown-toggle' data-toggle='dropdown'>
+					".$_SESSION['nombre']."
+					</button><div class='dropdown-menu'><a class='dropdown-item' href='cerrarSesion.php'>Cerrar sesion</a></div></div> ";
+				}?>		
+				</li>
+			  </ul>
+		</nav>	
+		<div class="cuerpo">
+			<h2 class="titInicio verde">Preguntas frecuentes </h2>
+			<ol type="1">
+			  <li>Coffee</li>
+			</ol>  
+¿Cual es la forma de pago?
+El pago se realiza en el momento de la entrega del pedido, solo se especifica previamente
+si se paga en efectivo o con tarjeta de modo de que se lleve el posnet al domicilio del cliente.
+¿Los pedidos sólo pueden ser realizados por internet?
+Para entrega a domicilio solo se pueden realizar compras online.
+¿Cuáles son los días y horarios de entrega de pedidos?
+El cliente especifica el horarios y dia de entrega cuando realiza el pedido,
+pero solo se entrega de 8 a 13hs.
+¿Puedo realizar un pedido fuera del horario de entrega? 
+Si,el pedido queda guardado y se envia en la fecha y hora especificada.
+¿Cual es el costo de envio?
+El costo es fijo de 200$, sin importar las distancias ni la cantidad de productos.
+¿Necesito tener un usuario registrado para realizar un pedido?
+Si, se piden datos basicos para tener un registro de quien realiza el pedido.
+
+
+		</div>
+		<footer>
+		<div class="footer-container">
+		  <div class="footer-main">
+			<div class="footer-columna">Supermercado SAV </div>
+			<div class="footer-columna">San Martin 1234. Rosario,Santa Fe.</div> 
+			<div class="footer-columna">Tel: (0341)-4322245</div>
+			</div>
+		  </div>
+		</footer>
+	</body>
+</html>
